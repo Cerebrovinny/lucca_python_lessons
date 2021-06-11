4acd3c04fab68ce3e1c0132a30ef0938039a44fd
Vinicius C <vinny.cardoso96@gmail.com>
2021-06-11 08:54:03 -0300

    Update basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -1,1 +1,77 @@
 
+#Diamonds lesson
+#John have 32 diamonds and found more 20 how many diamonds John have in total?
+#Use variables to do the sum
+
+
+#Calculate the value of x
+y = 10
+z = 15
+x = y + z
+
+#Calculate the value of z
+y = 26
+x = 38
+z = y + z
+
+#Calculate the value of a
+y = 45
+x = 16
+o = 12
+a = y + x + o
+
+#Calculate the value of e
+y = 14
+x = 19
+e = y + x
+
+#Calculate the value of z
+y = 24
+x = 35
+a = 40
+z = y + x + a
+
+#Calculate the value of b
+y = 26
+x = 38
+i = 40
+b = y + x + i
+
+#Calculate the value of z
+y = 56
+x = 12
+p = 23
+z = y + x + p
+
+#Calculate the value of b
+y = 34
+x = 43
+z = 32
+b = y + x + z
+
+#Calculate the value of a
+y = 26
+x = 38
+a = 32
+z = y + z
+
+#Calculate the value of x
+y = 12
+x = 21
+o = 12
+z = y + x
+
+#Calculate the value of a
+y = 50
+x = 31
+a = y + x
+
+#Calculate the value of b
+y = 26
+x = 38
+z = y + z
+
+#Calculate the value of z
+y = 14
+x = 19
+z = y + z
