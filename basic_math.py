4972d52b15006e30723fc44087b58cde1155d666
Lucca Vinicius Cardoso Carmelin <81098582+luccaccarmelin@users.noreply.github.com>
2021-06-11 18:48:31 -0300

    Update basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -2,76 +2,107 @@
 #Diamonds lesson
 #John have 32 diamonds and found more 20 how many diamonds John have in total?
 #Use variables to do the sum
+#R: 
+diamond_a = 32
+diamond_b = 20
+  
+print(diamond_a + diamond_b)
 
 
 #Calculate the value of x
 y = 10
 z = 15
 x = y + z
+print(x)
+#R: x = 25
 
 #Calculate the value of z
 y = 26
 x = 38
 z = y + z
+print(z)
+#R: z = undefined/error
 
 #Calculate the value of a
 y = 45
 x = 16
 o = 12
 a = y + x + o
+print(a)
+#R: a = 73
 
 #Calculate the value of e
 y = 14
 x = 19
 e = y + x
+print(e)
+#R: e = 33
 
 #Calculate the value of z
 y = 24
 x = 35
 a = 40
 z = y + x + a
+print(z)
+#R: z = 99
 
 #Calculate the value of b
 y = 26
 x = 38
 i = 40
 b = y + x + i
+print(b)
+#R: 104
 
 #Calculate the value of z
 y = 56
 x = 12
 p = 23
 z = y + x + p
+print(z)
+#R: z = 91
 
 #Calculate the value of b
 y = 34
 x = 43
 z = 32
 b = y + x + z
+print(b)
+#R: b = 109
 
 #Calculate the value of a
 y = 26
 x = 38
 a = 32
 z = y + z
+print(a)
+#R: a = undefined/error
 
 #Calculate the value of x
 y = 12
 x = 21
 o = 12
 z = y + x
+print(x)
+#R: x = 21
 
 #Calculate the value of a
 y = 50
 x = 31
 a = y + x
+print(a)
+#R: a = 81
 
 #Calculate the value of b
 y = 26
 x = 38
 z = y + z
+print(b)
+#R: b = undefined/error
 
 #Calculate the value of z
 y = 14
 x = 19
 z = y + z
+print(z)
+#R: undefined/error
