639f3d2ec6b92f5726472d7ec8b281892504987d
Vinicius C <vinny.cardoso96@gmail.com>
2021-06-12 22:30:18 -0300

    Update basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -14,14 +14,12 @@ y = 10
 z = 15
 x = y + z
 print(x)
-#R: x = 25
 
 #Calculate the value of z
 y = 26
 x = 38
 z = y + z
-print(z)
-#R: z = undefined/error
+#print(z)
 
 #Calculate the value of a
 y = 45
@@ -29,14 +27,13 @@ x = 16
 o = 12
 a = y + x + o
 print(a)
-#R: a = 73
+
 
 #Calculate the value of e
 y = 14
 x = 19
 e = y + x
 print(e)
-#R: e = 33
 
 #Calculate the value of z
 y = 24
@@ -44,7 +41,6 @@ x = 35
 a = 40
 z = y + x + a
 print(z)
-#R: z = 99
 
 #Calculate the value of b
 y = 26
@@ -52,7 +48,6 @@ x = 38
 i = 40
 b = y + x + i
 print(b)
-#R: 104
 
 #Calculate the value of z
 y = 56
@@ -60,7 +55,6 @@ x = 12
 p = 23
 z = y + x + p
 print(z)
-#R: z = 91
 
 #Calculate the value of b
 y = 34
@@ -68,15 +62,13 @@ x = 43
 z = 32
 b = y + x + z
 print(b)
-#R: b = 109
 
 #Calculate the value of a
 y = 26
 x = 38
 a = 32
 z = y + z
-print(a)
-#R: a = undefined/error
+#print(a)
 
 #Calculate the value of x
 y = 12
@@ -84,25 +76,21 @@ x = 21
 o = 12
 z = y + x
 print(x)
-#R: x = 21
 
 #Calculate the value of a
 y = 50
 x = 31
 a = y + x
 print(a)
-#R: a = 81
 
 #Calculate the value of b
 y = 26
 x = 38
 z = y + z
-print(b)
-#R: b = undefined/error
+#print(b)
 
 #Calculate the value of z
 y = 14
 x = 19
 z = y + z
-print(z)
-#R: undefined/error
+#print(z)
